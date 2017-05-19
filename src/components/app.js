8b72c150500d7f004f7694596ff448479bc75e03
Shivarudrappa <pavithra.shivarudrappa@fluke.com>
2017-05-19 11:06:48 +0530

    Added the  base code

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+
+class AppComponent extends Component{
+
+	render(){
+		return (
+				<div>
+					First Application
+				</div>
+			);
+	}
+}
+
+export default AppComponent;
